3348d22825dc649748998caa3da808ba9bc846a4
nigel-ip <nigel.hj.ip@gmail.com>
2026-08-12 20:21:20 -0700

    Camera Callibration

--- cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
+++ cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
@@ -205,9 +205,9 @@ public class OpenCvRaspberryPiCamera implements CubeColorInspector{
                         findColor(1500+offsetX,300+offsetY,150,150),
                 },
                 {
-                        findColor(950+offsetX,1160+offsetY,98,225),
+                        findColor(950+offsetX,1160+offsetY,88,256),
                         center,
-                        findColor(1550+offsetX,650+offsetY,230,150)
+                        findColor(1050+offsetX,700+offsetY,230,150)
                 },
                 {
                         findColor(730+offsetX,1725+offsetY,120,125),
